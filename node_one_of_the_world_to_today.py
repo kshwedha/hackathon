596ec3ee54a86e8da8fa738b8d74602f896b8718
maathmaath <42066081+maathmaath@users.noreply.github.com>
2021-03-06 18:02:25 +0530

    Delete node_one_of_the_world_to_today.py

--- node_one_of_the_world_to_today.py
+++
@@ -1,4 +0,0 @@
-#this program defines the actual evolution of advanced or future generation of mankind, from null or first node to the present neglgible branch of science.
-
-start->node=desire/thaught
-node->next=what_next???
