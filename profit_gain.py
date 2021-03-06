e5b50c77ec35726c5a0ab2ef43d5ddf2c75ab064
maathmaath <42066081+maathmaath@users.noreply.github.com>
2021-03-06 18:14:43 +0530

    Update profit_gain.py

--- profit_gain.py
+++ profit_gain.py
@@ -1,20 +1,34 @@
 #1 2 3 4 9 8
-#n = int(input())
+#input
+#constraint ith value < i+1th val to traverse for profit
+
 a=[]
 [a.append(int(i)) for i in input("enter\n").split(' ')]
 sum = 0
 for i, j in enumerate(a):
-    if i+1==len(a)-1:
-        sum+=a[i+1]
-        break
-    if a[i] < a[i+1]:
-        sum+= j
+    if i+1==len(a):
+        sum+=a[i]
     else:
-        pass
+        var_ = -1
+        for k in a[i+1:]:
+            if var > a[k]:
+                var_ = a[k]
+        sum+= var_
+        return sum
+
+
+def type2():
+    for i in a:
+        if sum < i:
+            sum+=i
+    return sum
+
+choice = ("which type 1 or 2")
+if ch == 1:
+    sum = type1()
+elif ch == 2:
+    sum = type2()
+else:
+    import os
+    sys.exit()    
 print(sum)
-
-
-#for i in a:
-#    if sum < i:
-#        sum+=i
-#print(sum)
